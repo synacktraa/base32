ee046afc44d90b14e29f3f8cb70d4a3ea34082a9
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-26 14:54:20 +0530

    updated header file

--- base32_utils.h
+++ base32_utils.h
@@ -1,18 +1,15 @@
-#ifndef UTILS_H /* Include guard */
-#define UTILS_H
+#ifndef BASE32_UTILS /* Include guard */
+#define BASE32_UTILS
 
 //Function declarations
 int isLower(int ch);
-char *Strcpy(char* destination, char* source);
 int Strlen(const char* string);
-void Strcat(char* destination, const char* source);
-int Strcmp(const char* string1, const char* string2);
-int Strncut(char* destination, char* source, int nCut);
 int insert(char*data, int index, int element, int uSize, int tSize);
 int delete(char*data, int index, int len);
-void memreset(char*data, int len);
+void reverse(char*str);
 int power(int base, int exp);
-void decToBin(int dec, char* bin);
+char* decToBin(int n);
 int binToDec(char* bin);
+char *basename(char const *path);
 
 #endif
